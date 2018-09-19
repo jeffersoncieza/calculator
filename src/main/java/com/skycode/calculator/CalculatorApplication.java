d5acbddc6bb78b67040520addc83b00b6085a08d
Jeferson Cieza <jefferson.cieza@pucp.edu.pe>
2018-09-19 11:59:37 -0500

    Static code analysis

--- src/main/java/com/skycode/calculator/CalculatorApplication.java
+++ src/main/java/com/skycode/calculator/CalculatorApplication.java
@@ -3,6 +3,9 @@ package com.skycode.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Boot application
+ */
 @SpringBootApplication
 public class CalculatorApplication {
 
