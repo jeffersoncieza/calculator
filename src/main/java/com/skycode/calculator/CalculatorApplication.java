cd33b670a10f27cd92dce36ec5bffd61f528efce
Jeferson Cieza <jefferson.cieza@pucp.edu.pe>
2018-09-19 10:55:39 -0500

    Add Spring Boot skeleton

---
+++ src/main/java/com/skycode/calculator/CalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.skycode.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+}
